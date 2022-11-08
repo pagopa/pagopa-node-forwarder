a789bbbe11a5fec58b6802b2d455fb5eda0fab1f
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-11-08 17:04:35 +0100

    Update set_kv_secrets.sh

--- scripts/set_kv_secrets.sh
+++ scripts/set_kv_secrets.sh
@@ -1,3 +1,3 @@
 
 az keyvault secret set --vault-name "pagopa-d-kv" --name "certificate-crt-node-forwarder" --file "certificate.crt"
-az keyvault secret set --vault-name "pagopa-d-kv" --name "certificate-key-node-forwarder" --file "certificate-key..pem"
+az keyvault secret set --vault-name "pagopa-d-kv" --name "certificate-key-node-forwarder" --file "certificate-key.pem"
