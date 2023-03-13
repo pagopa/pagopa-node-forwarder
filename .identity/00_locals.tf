777a1cdc498272703efa89e84e1ddfdfd3e6e302
Jacopo <jacopo.carlini@gmail.com>
2023-03-13 16:32:50 +0100

    [PAGOPA-763] opex: fix

--- .identity/00_locals.tf
+++ .identity/00_locals.tf
@@ -4,13 +4,4 @@ locals {
     repository = "pagopa-node-forwarder"
   }
 
-  azure = {
-    product = "pagopa-${var.env_short}"
-    project = "${local.azure.product}-${local.azure.domain.location_short}-${local.azure.domain.name}"
-
-    domain = {
-      name           = "apiconfig"
-      location_short = "weu"
-    }
-  }
 }
